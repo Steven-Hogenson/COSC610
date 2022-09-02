7f0ade40a6efd17e4d0aa8973f91cb70f9462454
Steven-Hogenson <stevenhogenson@gmail.com>
2022-09-02 13:19:44 -0400

    added comments

--- src/Kill2.java
+++ src/Kill2.java
@@ -5,11 +5,14 @@
 public class Kill2 {
     public static void main(String[] args) {
 
+        //gets input from command line
         String remove2 = args[0];
+        //condition if the inputted String is less than 2 characters in length, and exits the program
         if (remove2.length() < 2) {
             System.out.println("WARNING: Not enough characters.");
             System.exit(0);
         }
+        //duplicates original String and then removes the 2nd character, and then prints that new String
         StringBuilder sb = new StringBuilder(remove2);
         sb.deleteCharAt(1);
         System.out.println(sb);
