84f255fff9a75a392b3c6fbc5e89356053bbde52
Steven-Hogenson <stevenhogenson@gmail.com>
2022-09-19 15:25:44 -0400

    cleaned up

--- src/MM.java
+++ src/MM.java
@@ -1,96 +1,105 @@
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.util.ArrayList;
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.List;
 import java.util.Scanner;
 
 /**
- * @author Steven Hogenson on 9/8/2022
+ * @author Steven Hogenson on 9/16/2022
  * @project COSC610
  */
 public class MM {
     /**
-     *
      * @param args command line arguments
      * @throws IOException throws exception if the specified files are not found
      */
     public static void main(String[] args) throws IOException {
-        List<ArrayList<Integer>> a = readFile("matrixA.txt");
-        List<ArrayList<Integer>> b = readFile("matrixB.txt");
         FileWriter fw = new FileWriter("matrixAnswer.txt");
-        int[][] matrixA = new int[a.size()][a.get(0).size()];
-        int[][] matrixB = new int[b.size()][b.get(0).size()];
-        populateMatrix(a, matrixA);
-        populateMatrix(b, matrixB);
-        int matrixA_Cols = a.get(0).size();
-        int matrixB_Rows = b.size();
-        //if valid conditions are met, then multiplications can occur
-        if ((matrixA_Cols == matrixB_Rows)) {
-            int[][] multMatrix = new int[a.size()][b.get(0).size()];
-            int sum = 0;
-            for (int i = 0; i < a.size(); i++) {
-                for (int j = 0; j < b.get(0).size(); j++) {
-                    for (int k = 0; k < b.size(); k++) {
-                        sum += matrixA[i][k] * matrixB[k][j];
-                    }
-                    multMatrix[i][j] = sum;
-                    sum = 0;
-                }
-            }
-            for (int i = 0; i < a.size(); i++) {
-                for (int j = 0; j < b.get(0).size(); j++) {
-                    fw.write(multMatrix[i][j]+ " ");
-                    //System.out.print(multMatrix[i][j] + " ");
-                }
-                //System.out.println();
-                fw.write("\n");
-            }
-        } else {
-            //System.out.println("Cannot multiply these 2 matrices.");
-            fw.write("Cannot multiply these 2 matrices.");
+        String matrixA = "matrixA.txt";
+        String matrixB = "matrixB.txt";
+        int rowsA = getRows(matrixA);
+        int colsA = getColumns(matrixA);
+        int rowsB = getRows(matrixB);
+        int colsB = getColumns(matrixB);
+        //Condition to determine validity
+        if (colsA != rowsB || colsB != rowsA) {
+            fw.write("Invalid matrix dimensions; cannot multiply");
+            System.exit(0);
         }
-
+        int[][] arrayA = new int[rowsA][colsA];
+        populateMatrix(matrixA, arrayA);
+        int[][] arrayB = new int[rowsB][colsB];
+        populateMatrix(matrixB, arrayB);
+        //Multiplying the 2 matrices
+        int[][] arrayMultiply = new int[rowsA][colsB];
+        for (int i = 0; i < rowsA; i++) {
+            for (int j = 0; j < colsB; j++) {
+                for (int k = 0; k < colsA; k++) {
+                    arrayMultiply[i][j] += arrayA[i][k] * arrayB[k][j];
+                }
+            }
+        }
+        //Writing output to file
+        for (int[] i : arrayMultiply) {
+            for (int j : i) {
+                System.out.print(j + " ");
+                fw.write(j + " ");
+            }
+            System.out.println();
+            fw.write("\n");
+        }
         fw.flush();
         fw.close();
     }
 
-    public static List<ArrayList<Integer>> readFile(String fileName) throws IOException {
-        Scanner input = new Scanner(new File(fileName));
-        List<ArrayList<Integer>> a = new ArrayList<>();
-        int row = 0;
-        String[] cols = input.nextLine().split(" ");
-        a.add(row, new ArrayList<>());
-        for (String col : cols) {
-            try {
-                a.get(row).add(Integer.parseInt(col));
-            } catch (Exception ignored) {
-            }
+    /**
+     * Gets the total number of rows from the matrix from the text file
+     *
+     * @param fileName the text file that contains a matrix
+     * @return integer of the amount of rows
+     */
+    public static int getRows(String fileName)  {
+        List<String> lines;
+        try {
+            lines = Files.readAllLines(Paths.get(fileName));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         }
-        row++;
-
-        while (input.hasNextLine()) {
-            a.add(row, new ArrayList<>());
-            for (int j = 0; j < cols.length; j++) {
-                try {
-                    a.get(row).add(input.nextInt());
-                } catch (java.util.NoSuchElementException ignored) {
-                }
-            }
-            row++;
-        }
-        return a;
+        return lines.size();
     }
 
     /**
-     * populates the 2d arrays with the appropriate values
-     * @param listMatrix the lists generated from readFile()
-     * @param twoDimensionMatrix the 2d array to populate
+     * Gets the length of a row (number of columns) from the text file that contains a matrix
+     *
+     * @param fileName the text file that contains a matrix
+     * @return integer of the number of columns
+     * @throws FileNotFoundException if file not found
      */
-    static void populateMatrix(List<ArrayList<Integer>> listMatrix, int[][] twoDimensionMatrix) {
-        for (int i = 0; i < listMatrix.size(); i++) {
-            for (int j = 0; j < listMatrix.get(i).size(); j++) {
-                twoDimensionMatrix[i][j] = listMatrix.get(i).get(j);
+    public static int getColumns(String fileName) throws FileNotFoundException {
+        int lineLength;
+        BufferedReader br = new BufferedReader((new FileReader(fileName)));
+        String line;
+        try {
+            line = br.readLine();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        lineLength = line.replaceAll("\s+", "").length();
+        return lineLength;
+    }
+
+    /**
+     * Populates the matrices with values from the text files
+     *
+     * @param fileName name of the text file to get matrix from
+     * @param array    the 2d array to populate
+     * @throws FileNotFoundException if file is not found
+     */
+    public static void populateMatrix(String fileName, int[][] array) throws FileNotFoundException {
+        Scanner sc = new Scanner(new File(fileName));
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[0].length; j++) {
+                array[i][j] = sc.nextInt();
             }
         }
     }
